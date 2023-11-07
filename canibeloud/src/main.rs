c4bc3a175a0ec33e521a050f6576863146776911
sakisv <me@sakisv.net>
2023-11-07 19:44:16 +0200

    Format yes/no as html

--- canibeloud/src/main.rs
+++ canibeloud/src/main.rs
@@ -9,6 +9,8 @@ async fn hello() -> impl Responder {
     // source:
     // https://www.astynomia.gr/odigos-tou-politi/chrisimes-symvoules/diafores/poies-einai-oi-ores-koinis-isychias/
 
+    let no_answer = "<html><body><p>No</p></body></html>";
+    let yes_answer = "<html><body><p>Yes (but within reason)</p></body></html>";
     match now.month() {
         4..=9 => {
             let start_noon = now.with_hour(15).unwrap().with_minute(0).unwrap();
@@ -17,7 +19,7 @@ async fn hello() -> impl Responder {
             let start_night = now.with_hour(23).unwrap().with_minute(0).unwrap();
             let end_night = now.with_hour(7).unwrap().with_minute(0).unwrap();
             if (now >= start_noon && now <= stop_noon) || (now >= start_night || now <= end_night) {
-                return HttpResponse::Ok().body("No");
+                return HttpResponse::Ok().body(no_answer);
             }
         }
         _ => {
@@ -27,12 +29,12 @@ async fn hello() -> impl Responder {
             let start_night = now.with_hour(22).unwrap().with_minute(0).unwrap();
             let end_night = now.with_hour(7).unwrap().with_minute(30).unwrap();
             if (now >= start_noon && now <= stop_noon) || (now >= start_night || now <= end_night) {
-                return HttpResponse::Ok().body("No");
+                return HttpResponse::Ok().body(no_answer);
             }
         }
     }
 
-    HttpResponse::Ok().body("Yes (but within reason)")
+    HttpResponse::Ok().body(yes_answer)
 }
 
 #[actix_web::main]
